1286792fbbaff7eaf41a01e39159a661a9c4341c
vesu-dev <174601598+vesu-dev@users.noreply.github.com>
2025-11-17 17:21:14 +0000

    Push progress

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,11 +9,11 @@ sed -e "s~MAINNET_RPC_URL~$MAINNET_RPC_URL~g" \
     -e "s~BLOCK_NUMBER~2386336~g" \
     Scarb.toml.template > Scarb.toml
 
-snforge test "Test_2386336"
+snforge test --max-n-steps 100000000 "Test_2386336"
 
 # run tests at block 3494530
 sed -e "s~MAINNET_RPC_URL~$MAINNET_RPC_URL~g" \
     -e "s~BLOCK_NUMBER~3494530~g" \
     Scarb.toml.template > Scarb.toml
 
-snforge test "Test_3494530"
+snforge test --max-n-steps 100000000 "Test_3494530"
